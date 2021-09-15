3dab37455e80385626d72498018b8c2687f82dc8
Luther-Sparks <272786724@qq.com>
2021-09-16 00:28:44 +0800

    changed image recognition into tujian api

--- captcha.py
+++ captcha.py
@@ -1,11 +1,21 @@
 #coding=UTF-8
-import easyocr
-import io
-import numpy
-from PIL import Image
+import base64
+import json
+from utils import * 
 
 def read_captcha(img_byte):
-    image = numpy.array(Image.open(io.BytesIO(img_byte)))
-    reader = easyocr.Reader(['en'])
-    result = reader.readtext(image, detail = 0)
-    return result[0]
+    base64_data = base64.b64encode(img_byte)
+    b64 = base64_data.decode()
+    account_data = ReadJson('./config/tujian_account.json')
+    data = {
+        "username": account_data["username"], 
+        "password": account_data["password"],
+        "typeid": account_data["typeid"],
+        "image": b64
+    }
+    result = json.loads(requests.post(url="http://api.ttshitu.com/predict", json=data).text)
+    if result["success"]:
+        return result["data"]["result"]
+    else:
+        return result["message"]
+    return ""
